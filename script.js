cec404c71268f8bc05ca60ab4b97d46add5eb720
Liridon Loku <liridonloku@gmail.com>
2021-08-28 15:33:45 +0200

    Added keyboard support and fixed +/- sign

--- script.js
+++ script.js
@@ -22,11 +22,14 @@ function percent(a,b){
     return a/100*b;
 }
 
+
 let input = []; //the array to contains all user input elements
+let negative; //The +/- sign toggle
 let displayString;  //used to enhance the display of the input
 let lastOperatorWasEquals;   //additional logic if the last operator was "=", see case "equals" in the switch statement
 const expression = document.getElementById('expression');
 const history = document.getElementById('history');
+const sign = document.getElementById('sign');
 const buttons = document.querySelectorAll("button");
 buttons.forEach(button =>{
     button.addEventListener('click', e =>{
@@ -136,6 +139,14 @@ buttons.forEach(button =>{
                 }
                 break;
             case 'negative':
+                if(sign.textContent === ''){
+                    sign.textContent = '-'
+                    negative = true;
+                }
+                else{
+                    sign.textContent = '';
+                    negative = false;
+                }
                 break;
             case 'point':
                 if(input.length === 0 || endsWithOperator){
@@ -154,22 +165,26 @@ buttons.forEach(button =>{
                 }
                 break;
         }
-        console.log(input);
 
+        //Turn array to string for better display
         displayString = input.join('');
         expression.textContent = displayString;
 
 
 
 
-        // Resize the font based on input length
+        // Resize font based on input length
         if(expression.textContent.length > 20){
             expression.style.transitionDuration = '0.1s';
+            sign.style.transitionDuration = '0.1s';
             expression.style.fontSize = '16px';
+            sign.style.fontSize = '16px';
         }
         else{
             expression.style.transitionDuration = '0.1s';
             expression.style.fontSize = '24px';
+            sign.style.transitionDuration = '0.1s';
+            sign.style.fontSize = '24px';
         }
     });
 });
@@ -177,6 +192,11 @@ buttons.forEach(button =>{
 function calculateExpression(array){
     let operatorIndex = Math.max(array.indexOf('+'), array.indexOf('-'), array.indexOf('%'), array.indexOf('×'), array.indexOf('÷'));
     let firstArg = parseFloat(array.slice(0,operatorIndex).join(''));
+    if(negative){
+        firstArg = firstArg * (-1);
+        negative = false;
+        sign.textContent = '';
+    }
     let operator = array[operatorIndex];
     let secondArg = parseFloat(array.slice(operatorIndex+1).join(''));
     let result = 0;
@@ -200,10 +220,212 @@ function calculateExpression(array){
             console.log("Operator selection error");
             break;
     }
-    result = parseFloat(result.toString()).toPrecision(15); //to fix incorrect calculation with long float numbers
+    result = parseFloat(result.toString()).toPrecision(15);     //to fix incorrect calculation with long float numbers
     result = result.toString();
-    while(result.charAt(result.length-1)==='0' || result.charAt(result.length-1)==='.'){
+    while((result.includes('.') && result.charAt(result.length-1)==='0') || result.charAt(result.length-1)==='.'){
         result = result.slice(0,-1);
     }
     return result;
 }
+
+//Keyboard support
+document.addEventListener('keydown', e =>{
+    let id = e.key;
+    console.log(e);
+    let hasOperator = input.includes('+') || input.includes('-') || input.includes('%') || input.includes('×') || input.includes('÷');
+    let operatorIndex = Math.max(input.indexOf('+'), input.indexOf('-'), input.indexOf('%'), input.indexOf('×'), input.indexOf('÷'));
+    let lastDigit = input[input.length-1];
+    let endsWithOperator = lastDigit==='+' || lastDigit==='-' || lastDigit==='%' || lastDigit==='×' || lastDigit==='÷';
+    switch (id){
+        case 'Delete':
+            expression.textContent = '';
+            history.textContent = '';
+            input = [];
+            break;
+        case 'Backspace':
+            input.pop();
+            break;            
+        case '%':
+        case '+':
+        case '-':
+            if(hasOperator && endsWithOperator){
+                input.pop();
+                input.push(e.key);
+                lastOperatorWasEquals = false;
+            }
+            else if(hasOperator && !endsWithOperator){
+                history.textContent = displayString;
+                let newResult = calculateExpression(input);
+                input = [];
+                input = input.concat(newResult);
+                input.push(e.key);
+                lastOperatorWasEquals = false;
+            }
+            else if(input.length === 0){
+                //If the screen is empty, do nothing.
+            }
+            else{
+                input.push(e.key);
+                lastOperatorWasEquals = false;
+            }
+            break;
+        case '*':
+            if(hasOperator && endsWithOperator){
+                input.pop();
+                input.push('×');
+                lastOperatorWasEquals = false;
+            }
+            else if(hasOperator && !endsWithOperator){
+                history.textContent = displayString;
+                let newResult = calculateExpression(input);
+                input = [];
+                input = input.concat(newResult);
+                input.push('×');
+                lastOperatorWasEquals = false;
+            }
+            else if(input.length === 0){
+                //If the screen is empty, do nothing.
+            }
+            else{
+                input.push('×');
+                lastOperatorWasEquals = false;
+            }
+            break;
+        case '/':
+            if(hasOperator && endsWithOperator){
+                input.pop();
+                input.push('÷');
+                lastOperatorWasEquals = false;
+            }
+            else if(hasOperator && !endsWithOperator){
+                history.textContent = displayString;
+                let newResult = calculateExpression(input);
+                input = [];
+                input = input.concat(newResult);
+                input.push('÷');
+                lastOperatorWasEquals = false;
+            }
+            else if(input.length === 0){
+                //If the screen is empty, do nothing.
+            }
+            else{
+                input.push('÷');
+                lastOperatorWasEquals = false;
+            }
+            break;
+        case '=':
+        case 'Enter':
+            if(hasOperator && !endsWithOperator){
+                history.textContent = displayString;
+                let newResult = calculateExpression(input);
+                input = [];
+                input = input.concat(newResult);
+                lastOperatorWasEquals = true;
+            }
+            break;
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            if((input.length === 1 && input[0] === '0') || (hasOperator && input.slice(operatorIndex+1).length === 1 && input[operatorIndex+1] === '0')){
+                //If the first digit of either operand is 0, replace it.
+                input.pop();
+                input.push(e.key);
+            }   
+            else if(lastOperatorWasEquals){
+                input = [];
+                input.push(e.key);
+                lastOperatorWasEquals = false;
+            }
+            else{
+                if(!hasOperator && input.length >=15){
+                    alert('Calculator doesn\'t work with longer numbers');
+                    //First argument can't have more than 15 digits.
+                }
+                else if(hasOperator && input.slice(operatorIndex).length >=16){
+                    alert('Calculator doesn\'t work with longer numbers');
+                    //Second argument can't have more than 15 digits.
+                }
+                else{
+                    input.push(e.key);
+                }
+            }
+            break;
+        case '0':
+            if((input.length === 1 && input[0] === '0') || (hasOperator && input.slice(operatorIndex+1).length === 1 && input[operatorIndex+1] === '0')){
+                //Do nothing if the first digit of either operand is 0.
+            }                
+            else if(lastOperatorWasEquals){
+                input = [];
+                input.push(e.key);
+                lastOperatorWasEquals = false;
+            }
+            else{
+                if(!hasOperator && input.length >=15){
+                    alert('Calculator doesn\'t work with longer numbers');
+                    //First argument can't have more than 15 digits.
+                }
+                else if(hasOperator && input.slice(operatorIndex).length >=16){
+                    alert('Calculator doesn\'t work with longer numbers');
+                    //Second argument can't have more than 15 digits.
+                }
+                else{
+                    input.push(e.key);
+                }
+            }
+            break;
+        case 'n':
+            if(sign.textContent === ''){
+                sign.textContent = '-'
+                negative = true;
+            }
+            else{
+                sign.textContent = '';
+                negative = false;
+            }
+            break;
+        case '.':
+            if(input.length === 0 || endsWithOperator){
+                input.push('0','.');
+            }
+            else if(lastOperatorWasEquals){
+                input = [];
+                input.push('0','.');
+                lastOperatorWasEquals = false;
+            }
+            else if((!hasOperator && input.includes('.')) || (hasOperator && input.slice(operatorIndex).includes('.'))){
+                //If current operand already includes a point, don't add another.
+            }
+            else{
+                input.push('.');
+            }
+            break;
+        default:
+            break;
+    }
+
+    //Turn array to string for better display
+    displayString = input.join('');
+    expression.textContent = displayString;
+
+
+
+    // Resize font based on input length
+    if(expression.textContent.length > 20){
+        expression.style.transitionDuration = '0.1s';
+        sign.style.transitionDuration = '0.1s';
+        expression.style.fontSize = '16px';
+        sign.style.fontSize = '16px';
+    }
+    else{
+        expression.style.transitionDuration = '0.1s';
+        expression.style.fontSize = '24px';
+        sign.style.transitionDuration = '0.1s';
+        sign.style.fontSize = '24px';
+    }
+});
